a31a73a3e7a0ea5ad7314dff0bb93761f5799a6b
Isabel Artiga <isabelartigaspam@gmail.com>
2024-01-14 14:49:22 +0100

    Index genome/contaminants

--- scripts/index.sh
+++ scripts/index.sh
@@ -4,5 +4,12 @@
 # The STAR command is provided for you. You should replace the parts surrounded
 # by "<>" and uncomment it.
 
-# STAR --runThreadN 4 --runMode genomeGenerate --genomeDir <outdir> \
-# --genomeFastaFiles <genomefile> --genomeSAindexNbases 9
+target=$1
+output_directory=$2
+
+mamba install -y star
+
+mkdir -p res/$(basename ${target} .fasta)_idx
+
+STAR --runThreadN 4 --runMode genomeGenerate --genomeDir res/$(basename ${target} .fasta)_idx \
+--genomeFastaFiles ${target} --genomeSAindexNbases 9
