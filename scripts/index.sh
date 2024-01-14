d2e450b71f0e38682778464371700f09e68836e2
Isabel Artiga <isabelartigaspam@gmail.com>
2024-01-14 14:54:59 +0100

    Comments in index.sh script

--- scripts/index.sh
+++ scripts/index.sh
@@ -1,15 +1,16 @@
-# This script should index the genome file specified in the first argument ($1),
+# This script indexs the genome file specified in the first argument ($1),
 # creating the index in a directory specified by the second argument ($2).
 
-# The STAR command is provided for you. You should replace the parts surrounded
-# by "<>" and uncomment it.
-
+# Capture input parameters from command arguments
 target=$1
 output_directory=$2
 
+# Install STAR
 mamba install -y star
 
+# Create the output directory if it doesn´t exist
 mkdir -p res/$(basename ${target} .fasta)_idx
 
+# Index genome/contaminants
 STAR --runThreadN 4 --runMode genomeGenerate --genomeDir res/$(basename ${target} .fasta)_idx \
 --genomeFastaFiles ${target} --genomeSAindexNbases 9
