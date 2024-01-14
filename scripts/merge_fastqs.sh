ba6de003dadfa59c0971ccf91ba4221b9c462931
Isabel Artiga <isabelartigaspam@gmail.com>
2024-01-14 14:13:05 +0100

    Comments in merge_fastqs.sh script.

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -1,13 +1,16 @@
-# This script should merge all files from a given sample (the sample id(sid) is
+# This script merges all files from a given sample (the sample id(sid) is
 # provided in the third argument ($3)) into a single file, which should be
 # stored in the output directory specified by the second argument ($2).
 #
 # The directory containing the samples is indicated by the first argument ($1).
 
+# Capture input parameters from command arguments
 origen_directory=$1
 output_directory=$2
 sid=$3
 
+# Create output directory in case it doesn´t exist
 mkdir -p out/merged
 
+# Merge the fastqs from the same sample into a single file 
 cat ${origen_directory}/${sid}* > ${output_directory}/${sid}.fastq.gz
