c5fe96fb8b0611caf0b0d5c503a26d75aae310c6
Isabel Artiga <isabelartigaspam@gmail.com>
2024-01-18 14:32:01 +0100

    Create cleanup.sh script

---
+++ scripts/cleanup.sh
@@ -0,0 +1,14 @@
+
+if [ $# == 0 ]; then
+   rm -r res/* out/* log/* data/*.gz
+else
+   if [ "$@" =~ "data" ]; then
+	rm -r data/*gz
+   elif [ "$@" =~ "resources" ]; then
+	rm -r res/*
+   elif [ "$@" =~ "outputs" ]; then
+        rm -r out/*
+   elif [ "$@" =~ "logs" ]; then
+        rm -r log/*
+   fi
+fi
