2fc34d520700828619d0522062019226e05dd76b
Isabel Artiga <isabelartigaspam@gmail.com>
2024-01-18 14:36:51 +0100

    Correct cleanup.sh script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,13 +2,13 @@
 if [ $# == 0 ]; then
    rm -r res/* out/* log/* data/*.gz
 else
-   if [ "$@" =~ "data" ]; then
+   if [ "$@" == "data" ]; then
 	rm -r data/*gz
-   elif [ "$@" =~ "resources" ]; then
+   elif [ "$@" == "resources" ]; then
 	rm -r res/*
-   elif [ "$@" =~ "outputs" ]; then
+   elif [ "$@" == "outputs" ]; then
         rm -r out/*
-   elif [ "$@" =~ "logs" ]; then
+   elif [ "$@" == "logs" ]; then
         rm -r log/*
    fi
 fi
