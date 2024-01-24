893ebc1a994246b5c7105493a9b3268628b55863
Isabel Artiga <isabelartigaspam@gmail.com>
2024-01-24 14:24:27 +0100

    Change cleanup data

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,9 +1,9 @@
 
 if [ $# == 0 ]; then
-   rm -r res/* out/* log/* data/*.gz
+   rm -r res/* out/* log/* data/*.fastq*
 else
    if [ "$@" == "data" ]; then
-	rm -r data/*gz
+	rm -r data/*.fastq*
    elif [ "$@" == "resources" ]; then
 	rm -r res/*
    elif [ "$@" == "outputs" ]; then
